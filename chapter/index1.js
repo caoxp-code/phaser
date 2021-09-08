d45a0b9073370c6aae6f83bab41d6c15cdb78270
曹晓鹏 <caoxiaopeng@xdf.cn>
2021-09-08 17:51:36 +0800

    实现第一个例子代码

---
+++ chapter/index1.js
@@ -0,0 +1,28 @@
+class ScenePage extends Phaser.Scene {
+  constructor () {
+    super()
+  }
+  preload () {
+    // 加载背景图片资源
+    this.load.image('bg', 'assets/sky.png')
+  }
+  create () {
+    // 将图片添加到scene中
+    this.add.image(400, 300, 'bg')
+    // 添加文本信息
+    this.add.text(400, 32, 'Hello,World', {color: '#00ff00'})
+  }
+  update () {
+
+  }
+}
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#2d2d2d',
+  parent: 'phaser-example',
+  scene: [ScenePage],
+  title: 'action game'
+}
+const game = new Phaser.Game(config)
